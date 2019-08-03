83af34cca56ed0c9fc08de6885b686f41798fe70
Barrington <Barringtonthorpe@gmail.com>
2019-08-03 15:51:02 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: Container(),
+      ),
+    ),
+  );
+}
+class name extends StatelessWidget {
+  const name({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: child,
+    );
+  }
+}
